1365dd03ddf74b5b8e6ee97d94aa58502868f274
relampagoUchiha <relampagouchiha21@gmail.com>
2022-10-22 15:24:53 -0300

    envio de arquivos iniciais

---
+++ README.md
@@ -0,0 +1,2 @@
+# Censuras-do-TSE
+Aplicação contendo uma API com todas as referências a censuras praticadas pelo Tribunal Superior Eleitoral do Brasil.  Viva a liberdade de expressão das pessoas e da mídia! 📣
---
+++ script/script.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Relampago Uchiha
+
+const getList = async () => {
+    const response = await
+    fetch()
+    return response.json(`https://my-json-server.typicode.com/RelampagoUchiha/demo/posts`)
+}
